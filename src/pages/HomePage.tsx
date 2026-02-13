ad1772125b534cd6e70a27ad7bc60de229a96f78
Zuhairarif <162948157+Zuhairarif@users.noreply.github.com>
2026-02-13 15:30:44 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -96,7 +96,7 @@ export default function HomePage() {
         <img
           src={ssnLogo}
           alt="SSN Logo"
-          className="absolute top-3 left-3 h-15 w-15 rounded-full object-contain"
+          className="absolute top-3 left-3 h-10 w-10 rounded-full object-contain"
         />
         <Link
           to="/admin/login"
