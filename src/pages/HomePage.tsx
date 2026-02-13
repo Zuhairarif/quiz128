5a75f183524a32304eb2358d110aa03ec6709461
Zuhairarif <162948157+Zuhairarif@users.noreply.github.com>
2026-02-13 15:23:24 +0530

    Update logo size in HomePage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -96,7 +96,7 @@ export default function HomePage() {
         <img
           src={ssnLogo}
           alt="SSN Logo"
-          className="absolute top-3 left-3 h-40 w-40 rounded-full object-contain"
+          className="absolute top-3 left-3 h-25 w-25 rounded-full object-contain"
         />
         <Link
           to="/admin/login"
